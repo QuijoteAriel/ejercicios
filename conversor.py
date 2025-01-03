b6f121d3c4f2fa8a55200b30c58c8c97a26eea0b
jose <jlfp15@gmail.com>
2025-01-03 12:12:41 -0300

    ejercicios conversor

--- conversor.py
+++ conversor.py
@@ -3,4 +3,32 @@ Convertidor de Unidades:
 Descripción: Un programa que convierte unidades de medida, como centímetros a pulgadas o grados Celsius a Fahrenheit.
 Conceptos a Practicar: Funciones, manejo de flotantes, manejo de cadenas.
 '''
+ 
 
+def conversor():
+    print('''que quieres convertir ??
+        1. cm a pulgadas 
+        2. grados centigrados a grador farenhied
+          ''')
+    
+    while True:
+        conv = float(input('opcion : '))
+        if conv == 1:
+            cm =float(input('centimetro '))
+            pulgadas = cm / 2.54
+            print(f'{cm} centimetros en igual a' , round(pulgadas,2))
+    
+        elif conv == 2:
+            pul = float(input('pulgadas : '))
+            centimetro = pul * 2.54
+            print(f'{pul} centimetros en igual a' , round(centimetro,2))
+        
+        elif conv == 0:
+            print('saliendo ')
+            break
+        else:
+            print('ingrese una opcion valida')
+            continue
+
+if __name__ == '__main__':
+    conversor()
--- generador.py
+++ generador.py
@@ -1,7 +1,23 @@
 import random
+import statistics
+from colorama import init, Style, Fore 
 
-lista = [random.randint(1, 100) for _ in range(10)]
+lista = [random.randint(1, 100) for _ in range(100)]
 print("Lista:", lista)
 print("Máximo:", max(lista))
 print("Mínimo:", min(lista))
 print("Media:", sum(lista) / len(lista))
+print("Mediana:", str(len(lista) /2 ))
+moda = statistics.mode(lista)
+print("Moda:", moda)
+
+def imprimir(lista):
+    moda = statistics.mode(lista)
+    for numero in lista:
+        if numero == moda:
+            print(Fore.GREEN + str(numero), end=" ")
+        else:
+            print(numero, end=" ")
+    print(Style.RESET_ALL)
+
+imprimir(lista)

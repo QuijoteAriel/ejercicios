35395cc6b73c10525c9f71be67f6f7c281e3d785
jose <jlfp15@gmail.com>
2025-06-04 17:10:07 -0300

    ejecicio main

---
+++ main.py
@@ -0,0 +1,17 @@
+
+clientes = ' jose, luis, '
+
+def create_cliente(cliente_name):
+    global clientes
+    clientes += cliente_name
+    _add_comma()
+
+def _add_comma():
+    global clientes
+    
+    clientes += ',' 
+
+
+if __name__=='__main__':
+    create_cliente('xioma, vian , teresa , fabio')
+    print(clientes)

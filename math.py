608af6c6ab2c03633bdffddda99665d6b35c92e8
jose <jlfp15@gmail.com>
2025-06-08 18:13:33 -0300

    actualizacion

--- math.py
+++ math.py
@@ -1,8 +1,8 @@
-import math
+#import math
 
-radio= -3
-area = math.pi *radio**2
-perimetro = 2* math.pi *radio
+#radio= 3
+#area = math.pi *radio**2
+#perimetro = 2* math.pi *radio
 
-print(area)
-print(perimetro)
+#3print(area)
+#print(perimetro)

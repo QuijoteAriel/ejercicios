8aa5bb68efa32daa4eb274663ad5d06477e63acf
jose <jlfp15@gmail.com>
2025-08-16 14:49:37 -0300

    update

---
+++ tipo_datos.py
@@ -0,0 +1,13 @@
+def found_type(value):
+    return type(value)
+
+reponse = found_type(1)
+print(reponse)
+
+response = found_type("jose")
+print(reponse)
+
+response = found_type(False)
+print(reponse)
+
+
